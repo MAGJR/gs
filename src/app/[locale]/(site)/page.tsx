8e4b548675f4e3cc6c8e8d4093ef90b3789458e7
MARCO ANTONIO GONZALES JUNIOR <102200335+MAGJR@users.noreply.github.com>
2024-05-25 08:03:20 -0300

    refactor: move introduction to a separete file

--- src/app/[locale]/(site)/page.tsx
+++ src/app/[locale]/(site)/page.tsx
@@ -17,36 +17,13 @@ import Logo from "./_components/logo"
 import Home from '../../../../public/assets/Home.jpg'
 import Header from "./_components/header"
 import Footer from "./_components/footer"
+import Product from "./_components/product"
 
 
 export default function Page() {
   
-  const [parent] = useAutoAnimate({duration: 4000})
+  
   const t = useTranslations()
-    const product = [
-        {
-            id: 1,
-            title: t("site.main.product.product01.title"),
-            description: t("site.main.product.product01.description"),
-            img: Produto,
-            price: t("site.main.product.product01.price")
-        },
-        {
-            id: 2,
-            title: t("site.main.product.product02.title"),
-            description: t("site.main.product.product02.description"),
-            img: Produto1,
-            price: t("site.main.product.product02.price")
-        },
-        {
-            id: 3,
-            title: t("site.main.product.product03.title"),
-            description: t("site.main.product.product03.description"),
-            img: Product3,
-            price: t("site.main.product.product03.price"),
-            
-        }
-    ]
    
     
   return (
@@ -81,49 +58,7 @@ export default function Page() {
           </div>
         </div>
       </section>
-      <section className="py-12 md:py-20">
-        <div className="container px-4 md:px-8">
-             
-
-          <h2 className="text-2xl md:text-3xl font-bold mb-8">{t("site.main.title")}</h2>
-          <div ref={parent} className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          {product.map((products) => (
-            <Card  key={products.id}>
-              <div className="flex items-center justify-center">
-                <Image
-                alt="Products"
-                className="rounded-t-lg "
-                height={200}
-                src={products.img}
-                style={{
-                  aspectRatio: "250/200",
-                  objectFit: "cover",
-                }}
-                
-                width={300}
-              />
-              </div>
-              <CardContent className="pt-4 grid items-center justify-center">
-                <h3 className="text-lg font-bold mb-2">{products.title}</h3>
-                <small className="text-gray-600 mb-4">{products.description}</small>
-                <div className="flex items-center justify-between">
-                
-        
-          <div className="flex items-center gap-0.5">
-            {product.map(start => (
-              <StarIcon key={start.id} className="w-5 h-5 fill-primary" />
-            ))}
-            
-            <StarIcon className="w-5 h-5 fill-muted stroke-muted-foreground" />
-            </div>
-                  <Button className="justify-end" size="sm">{t("site.button.title")}</Button>
-                </div>
-              </CardContent>
-            </Card>
-          ))}
-          </div>
-        </div>
-      </section>
+      <Product />
       <section className="bg-gray-100 py-12 md:py-20">
         <div className="container px-4 md:px-8">
           <h2 className="text-2xl md:text-3xl font-bold mb-8">{t("site.main.product.testimonials.title")}</h2>
