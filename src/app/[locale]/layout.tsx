22be4fee0a6038aa8c0823b09b404d48ec7f6426
MARCO ANTONIO GONZALES JUNIOR <102200335+MAGJR@users.noreply.github.com>
2024-07-13 14:24:29 -0300

    feature: layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,12 +1,18 @@
 import { NextIntlClientProvider, useMessages } from 'next-intl'
 import Footer from '../components/footer'
 import Header from '../components/header'
+import { Metadata } from 'next'
 
 type Props = {
   children: React.ReactNode
   params: {
     locale: 'en' | 'pt-BR'
   }
+}
+
+export const metadata: Metadata = {
+  title: 'GS | Products ',
+  description: 'Get the best products all in one place',
 }
 
 export default function Layout({ children, params: { locale } }: Props) {
